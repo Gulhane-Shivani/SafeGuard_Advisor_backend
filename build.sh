e1f98c8d986ea82a98d00c310f473db8986da59a
Gulhane-Shivani <shivanigulhane2002@gmail.com>
2026-04-16 17:00:53 +0530

    Fix Render build by configuring Cargo home and updating Python version

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 # Create a writable cargo directory to avoid "Read-only file system" errors
-mkdir -p /opt/render/project/src/.cargo
-export CARGO_HOME=/opt/render/project/src/.cargo
+mkdir -p ./.cargo
+export CARGO_HOME=$(pwd)/.cargo
 
 # Upgrade pip
 python -m pip install --upgrade pip
